db1798b753fecc686dd1bfcc415f7700df11fa11
ZzzGin <13491630+ZzzGin@users.noreply.github.com>
2022-05-22 15:29:28 -0700

    amplify push

---
+++ src/graphql/queries.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getDiode = /* GraphQL */ `
+  query GetDiode($id: ID!) {
+    getDiode(id: $id) {
+      id
+      state
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listDiodes = /* GraphQL */ `
+  query ListDiodes(
+    $filter: ModelDiodeFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listDiodes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        state
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateDiode = /* GraphQL */ `
+  subscription OnCreateDiode {
+    onCreateDiode {
+      id
+      state
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateDiode = /* GraphQL */ `
+  subscription OnUpdateDiode {
+    onUpdateDiode {
+      id
+      state
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteDiode = /* GraphQL */ `
+  subscription OnDeleteDiode {
+    onDeleteDiode {
+      id
+      state
+      createdAt
+      updatedAt
+    }
+  }
+`;
